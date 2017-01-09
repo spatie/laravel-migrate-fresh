600104a98350b1d0d8046e1c0a72c8684d19eade
freek <freek@spatie.be>
2017-01-09 14:45:01 +0100

    wip

--- tests/MigrateFreshTest.php
+++ tests/MigrateFreshTest.php
@@ -5,7 +5,7 @@ namespace Spatie\MigrateFresh\Test;
 class MigrateFreshTest extends TestCase
 {
     /** @test */
-    public function it_tests()
+    public function it_can_run_without_any_migrations_present()
     {
         $this->assertTrue(true);
     }
---
+++ tests/database/migrations/2017_01_01_000000_create_dummy_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersBackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dummy', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('text');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users_back');
+    }
+}
