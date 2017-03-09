2b8e59a49abb3b8eea65eb6e64531820528d8afe
Jakub Klapka <jakub-klapka@users.noreply.github.com>
2017-03-09 21:30:31 +0100

    Add support for MS SQL Server (#32)
    * Add SQL Server Table Dropper
    
    * Fix CI styles
    
    * More style fixes
    
    * Style fixes
    
    * Move sql script to dropper class
    
    * Fix unintended tab
    
    * Another style fix

---
+++ src/TableDroppers/Sqlsrv.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Spatie\MigrateFresh\TableDroppers;
+
+use DB;
+
+class Sqlsrv implements TableDropper
+{
+    private $constraintDropScript = '
+        DECLARE @Sql NVARCHAR(500) DECLARE @Cursor CURSOR
+        SET @Cursor = CURSOR FAST_FORWARD FOR
+        SELECT DISTINCT sql = \'ALTER TABLE [\'+tc2.CONSTRAINT_SCHEMA+\'].[\' + tc2.TABLE_NAME + \'] DROP [\' + rc1.CONSTRAINT_NAME + \']\'
+        FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS rc1
+        LEFT JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc2 ON tc2.CONSTRAINT_NAME =rc1.CONSTRAINT_NAME
+        
+        OPEN @Cursor FETCH NEXT FROM @Cursor INTO @Sql
+        
+        WHILE (@@FETCH_STATUS = 0)
+        BEGIN
+        PRINT @Sql
+        Exec (@Sql)
+        FETCH NEXT FROM @Cursor INTO @Sql
+        END
+        
+        CLOSE @Cursor DEALLOCATE @Cursor';
+
+    public function dropAllTables()
+    {
+        DB::unprepared($this->constraintDropScript);
+        DB::unprepared("exec sp_MSforeachtable 'DROP TABLE ?'");
+    }
+}
